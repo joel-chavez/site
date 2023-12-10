f51f184b49e1d642a60df9bbc62e212e23ec7b23
Joel Chavez <jc3270@georgetown.edu>
2023-12-10 13:01:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var sections = document.querySelectorAll('.animate-on-scroll');
-
   
+    var sections = document.querySelectorAll('.animate-on-scroll');
     // Your existing code for section animations
     var observer = new IntersectionObserver(function(entries, observer) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
           if (!entry.target.classList.contains('animate')) {
-            entry.target.classList.add('animate');
+            entry.target.classList.add('animate','fixedImage');
           }
         } else {
-          entry.target.classList.remove('animate');
+          entry.target.classList.remove('animate','fixedImage');
         }
       });
-    }, { threshold: 0 });
+    }, { threshold: 0.5 });
   
     sections.forEach(function(section) {
       observer.observe(section);
     });
-  
-
-  });
+});
